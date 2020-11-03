4246fc1d8a48a1fc72a23350151779e652f5dc41
WMXPY <wm@mengw.io>
2020-11-03 14:54:28 -0600

    finsih debug log mixin

--- src/debug-log.ts
+++ src/debug-log.ts
@@ -4,15 +4,48 @@
  * @description Debug log
  */
 
-import { Barktler, BarktlerMixin } from "@barktler/core";
+import { Barktler, BarktlerMixin, IRequestConfig, IResponseConfig } from "@barktler/core";
 
 export type DebugLogMixinOptions = {
 
+    readonly isDevelopment?: () => boolean;
+    readonly requestHeader?: string;
+    readonly responseHeader?: string;
 };
 
-export const createDebugLogMixin: () => BarktlerMixin = (options: DebugLogMixinOptions = {}) => {
+export const createDebugLogMixin: () => BarktlerMixin = (
+    options: DebugLogMixinOptions = {
+        isDevelopment: () => {
+            if (!process.env.NODE_ENV) {
+                return false;
+            }
+            return process.env.NODE_ENV.toString().toUpperCase() === 'DEVELOPMENT';
+        },
+        requestHeader: "DEBUG REQUEST ➡",
+        responseHeader: "DEBUG RESPONSE ☑",
+    },
+) => {
 
     return (instance: Barktler) => {
 
+        instance.preHook.sideEffect.add((request: IRequestConfig) => {
+            if (options.isDevelopment()) {
+                console.log(
+                    options.requestHeader,
+                    request.url,
+                    request.headers,
+                    request.body,
+                );
+            }
+        });
+        instance.postHook.sideEffect.add((response: IResponseConfig) => {
+            if (options.isDevelopment()) {
+                console.log(
+                    options.responseHeader,
+                    response.headers,
+                    response.data,
+                );
+            }
+        });
     };
 };
